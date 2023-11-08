1d13604aaa8ed46b6736f9e68fc478adc3238d7c
Adam Sobczyk <adamvs@stud.ntnu.no>
2023-11-08 14:42:20 +0100

    copy footers

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-/*const topNav = document.getElementById("topnav1").innerHTML; 
-sessionStorage.setItem("topNavSave", topNav);
-document.getElementById("topnav").innerHTML=sessionStorage.getItem("topNavSave");*/
+//TopNav
 if (sessionStorage.getItem("topNavSave")) {
     document.getElementById("topnav").innerHTML = sessionStorage.getItem("topNavSave");
 }
 const topNav1Content = document.getElementById("topnav1").innerHTML;
 sessionStorage.setItem("topNavSave", topNav1Content);
+
+//Footer
+if (sessionStorage.getItem("footerSave")) {
+    document.getElementById("footer").innerHTML = sessionStorage.getItem("footerSave");
+}
+const footer1Content = document.getElementById("footer1").innerHTML;
+sessionStorage.setItem("footerSave", topNav1Content);
