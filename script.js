e02d766bb8899b10ecba97bb91d3b40b93bd6ee9
Adam Sobczyk <adamvs@stud.ntnu.no>
2023-11-09 16:52:41 +0100

    Fix Bugs, Copy nav & footers

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-//TopNav
-if (sessionStorage.getItem("topNavSave")) {
+const tag = document.getElementsByTagName("title");
+const title = tag[0].innerHTML;
+
+//TOPNAV
+//Paste from save
+if (sessionStorage.getItem("topNavSave") && title != "Homepage") {
     document.getElementById("topnav").innerHTML = sessionStorage.getItem("topNavSave");
 }
-const topNav1Content = document.getElementById("topnav1").innerHTML;
-sessionStorage.setItem("topNavSave", topNav1Content);
+//Copy & save
+else {
+    const topNav1Content = document.getElementById("topnav1").innerHTML;
+    sessionStorage.setItem("topNavSave", topNav1Content);
+}
 
-//Footer
-if (sessionStorage.getItem("footerSave")) {
+//FOOTER
+//Paste from save
+if (sessionStorage.getItem("footerSave") && title != "Homepage") {
     document.getElementById("footer").innerHTML = sessionStorage.getItem("footerSave");
 }
-const footer1Content = document.getElementById("footer1").innerHTML;
-sessionStorage.setItem("footerSave", topNav1Content);
+//Copy & save
+else {
+    const footer1Content = document.getElementById("footer1").innerHTML;
+    sessionStorage.setItem("footerSave", footer1Content);
+}
+if (title == "Cart") {
+    const cart = document.getElementById("footer");
+    cart.setAttribute("id", "footer-cart");
+}
+if (title == "Menu" || title == "Locations" || title == "Work At Frydays" || title == "About Us" || title == "Application Form") {
+    const file = document.getElementById("footer");
+    file.setAttribute("id", "scrollFooters");
+}
