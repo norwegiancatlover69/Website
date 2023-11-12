fae7be653eda815e7803c9b47b2ca75c0ca8b18b
Krugg1 <128526719+Krugg1@users.noreply.github.com>
2023-11-12 03:37:35 +0100

    8

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-//COPY TOPNAV AND FOOTER
-//Title names to identify current file
-const tag = document.getElementsByTagName("title");
-const title = tag[0].innerHTML;
-
 //TOPNAV
-document.getElementById("topnav").innerHTML = '<a class="logoLink" href="homepage.html"><img id="logo" class="logo" src="img/frydays.png" alt="Frydays logo"></a><a class="l1" href="menu.html">Order Now</a><a class="l2" href="locations.html">Locations</a><a class="l3" href="work.html">Work at<br>Frydays</a><a class="l4" href="contact.html">Contact Us</a><a class="l5" href="about.html">About Us</a><a class="l6" href="studDiscount.html">Student<br>Discount</a>';
+document.getElementById("topnav").innerHTML = '<span class="closebtn" id="closebtn" onclick="closeNav()">&times;</span><a class="logoLink" id="logoLink" onclick="closeNav()" href="homepage.html"><img id="logo" class="logo" src="img/frydays.png" alt="Frydays logo"></a><a id="l1" href="menu.html">Order Now</a><a id="l2" href="locations.html">Locations</a><a id="l3" href="work.html">Work at<br>Frydays</a><a id="l4" href="contact.html">Contact Us</a><a id="l5" href="about.html">About Us</a><a id="l6" href="studDiscount.html">Student<br>Discount</a>';
 
 //FOOTER
 document.getElementById("footer").innerHTML = '<p class="copyright">Copyright © Frydays.com - All Rights Reserved</p><div class="social_media"><p>Follow us on social media!</p><div><button id="instagram"><img class="social_media_buttons" src="img/instagram-icon.png" alt="Instagram"></button><button id="twitter"><img class="social_media_buttons" src="img/twitter-icon.png" alt="Twitter"></button><button id="tiktok"><img class="social_media_buttons" src="img/tiktok-icon.png" alt="Tiktok"></button></div></div>';
@@ -14,7 +9,6 @@ if (title == "Cart") {
     const cart = document.getElementById("footer");
     cart.setAttribute("id", "footer-cart");
 }
-
 
 //TOPNAV MEDIA QUERIES
 function changeImg(check) {
@@ -32,9 +26,15 @@ check.addListener(changeImg);
 function changeNav(check2) {
     if (check2.matches) {
         document.getElementById("topnav").setAttribute("id", "leftnav");
+        document.getElementById("logo").setAttribute("src", "img/frydays.png");
+        document.getElementById("leftnavIcon").style.display="block";
+        closeNav();
     }
     else if (document.getElementById("leftnav")) {
+        openNav();
         document.getElementById("leftnav").setAttribute("id", "topnav");
+        document.getElementById("logo").setAttribute("src", "img/frydaysNew.png");
+        document.getElementById("closebtn").style.display="none";
     }
 }
 var check2 = window.matchMedia("(max-width: 700px)");
@@ -42,12 +42,31 @@ changeNav(check2);
 check2.addListener(changeNav);
 
 function openNav() {
-    document.getElementById("leftnav").style.width = "100vh";
+    document.getElementById("leftnav").style.width = "100vw";
+    document.getElementById("leftnavIcon").style.display = "none";
+    document.getElementById("logo").style.display="block";
+    document.getElementById("closebtn").style.display="block";
+    document.getElementById("l1").style.display= "block";
+    document.getElementById("l2").style.display= "block";
+    document.getElementById("l3").style.display= "block";
+    document.getElementById("l4").style.display= "block";
+    document.getElementById("l5").style.display= "block";
+    document.getElementById("l6").style.display= "block";
+    document.getElementById("logoLink").style.display= "block";
 }
 function closeNav() {
     document.getElementById("leftnav").style.width = "0";
+    document.getElementById("leftnavIcon").style.display = "block";
+    document.getElementById("logo").style.display="none";
+    document.getElementById("closebtn").style.display="none";
+    document.getElementById("l1").style.display= "none";
+    document.getElementById("l2").style.display= "none";
+    document.getElementById("l3").style.display= "none";
+    document.getElementById("l4").style.display= "none";
+    document.getElementById("l5").style.display= "none";
+    document.getElementById("l6").style.display= "none";
+    document.getElementById("logoLink").style.display= "none";
 }
-
 // Slideshow CSS
 let slideIndex = 1;
 showSlides(slideIndex);
