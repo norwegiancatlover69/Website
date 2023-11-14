406f9360806dfca9526b1a25e340d4958c0a3735
norwegiancatlover69 <148864202+norwegiancatlover69@users.noreply.github.com>
2023-11-14 15:18:33 +0100

    Add files via upload

---
+++ menuAddItem.js
@@ -0,0 +1,177 @@
+let counter = 0; 
+
+function addToCartClassicBurger() {
+    if (sessionStorage.getItem("Classic Burger")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Classic Burger", "Classic Burger");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+
+}
+function addToCartJuicyDouble() {
+    if (sessionStorage.getItem("Juicy Double")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Juicy Double", "Juicy Double");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    } 
+}
+function addToCartBaconator() {
+    if (sessionStorage.getItem("Baconator")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Baconator", "Baconator");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+
+
+
+function addToCartClassicSand() {
+    if (sessionStorage.getItem("Classic Sandwich")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Classic Sandwich", "Classic Sandwich");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+function addToCartOmeletteSand() {
+    if (sessionStorage.getItem("Omelette Sandwich")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Omelette Sandwich", "Omelette Sandwich");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+function addToCartEggSand() {
+    if (sessionStorage.getItem("Egg Sandwich")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Egg Sandwich", "Egg Sandwich");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+
+
+
+function addToCartAvocadoCrunch() {
+    if (sessionStorage.getItem("Avocado Crunch")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Avocado Crunch", "Avocado Crunch");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+function addToCartAvocadoSpicySalsa() {
+    if (sessionStorage.getItem("Spicy Salsa")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Spicy Salsa", "Spicy Salsa");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+function addToCartAvocadoClassicChicken() {
+    if (sessionStorage.getItem("Classic Chicken")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Classic Chicken", "Classic Chicken");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+
+
+function addToCartPotato() {
+    if (sessionStorage.getItem("Potato Fries")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Potato Fries", "Potato Fries");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+function addToCartSweetPotato() {
+    if (sessionStorage.getItem("Sweet Potato Fries")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Sweet Potato Fries", "Sweet Potato Fries");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+
+
+function addToCartCola() {
+    if (sessionStorage.getItem("Coca-Cola")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Coca-Cola", "Coca-Cola");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+function addToCartFanta() {
+    if (sessionStorage.getItem("Fanta")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Fanta", "Fanta");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+function addToCartSprite() {
+    if (sessionStorage.getItem("Sprite")){
+        alert("Product Already added in Cart");
+
+    }
+    else{
+        sessionStorage.setItem("Sprite", "Sprite");
+        counter += 1; 
+        document.getElementById("cartItems").innerHTML = counter;
+    }
+}
+
+
+
+
+
+
+
+
+
+
