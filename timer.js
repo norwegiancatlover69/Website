c4907ad0298253046506bfda82622fe2bd474e40
BuildTools <unconfigured@null.spigotmc.org>
2023-11-16 02:32:18 +0100

    timer ferdig

--- timer.js
+++ timer.js
@@ -1,22 +1,29 @@
-var minTimeLimit = 10;
-var secTimeLimit = minTimeLimit * 60;
-var timer1 = document.getElementById("timer");
-
+//https://codepen.io/luci/pen/EEavVR
+//Random time from 10 to 20 minutes
+let randNumber = Math.floor(Math.random()*11) + 10;
+//Timer
+document.getElementById("timer").innerHTML = randNumber + ":00";
+let minTimeLimit = randNumber
+let secTimeLimit = minTimeLimit * 60;
+let timer1 = document.getElementById("timer");
+//Update
 function startTimer() {
     secTimeLimit--;
-    var minutes = Math.floor(secTimeLimit / 60);
-    var seconds = secTimeLimit % 60;
+    let minutes = Math.floor(secTimeLimit / 60);
+    let seconds = secTimeLimit % 60;
     if (secTimeLimit < 0) {
-    timer1.textContent = "00:00";
-    clearInterval(timerInterval);
-    return;
-}
-if (minutes < 10) {
-    minutes = "0" + minutes;
-}
-if (seconds < 10) {
-    seconds = "0" + seconds;
-}
-timer1.textContent = minutes + ":" + seconds;
-}
-var timerInterval = setInterval(startTimer, 1000);
+        timer1.innerHTML = "00:00";
+        clearInterval(timerInterval);
+        alert("Your food is ready!")
+        return;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    timer1.innerHTML = minutes + ":" + seconds;
+    }
+//Update every second
+let timerInterval = setInterval(startTimer, 1000);
